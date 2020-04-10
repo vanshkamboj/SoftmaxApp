933d3ace36cdb76a5b0b0f4693d010da1b6710d4
vanshkamboj <vanshkamboj.mca@gmail.com>
2020-04-10 16:32:39 +0530

    add push notification

--- src/App.js
+++ src/App.js
@@ -8,14 +8,32 @@ import {
 } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import PushController from './components/PushController'
 import reducers from './reducers'
 import Router from "./Router"
 import ReduxThunk from 'redux-thunk'
+// let pushData = [
+//     {
+//       title: "First push",
+//       message: "First push message"
+//     },
+//     {
+//       title: "Second push",
+//       message: "Second push message"
+//     }
+//   ]
+//   _renderItem = ({ item }) => (
+//     <View key={item.title}>
+//       <Text style={styles.title}>{item.title}</Text>
+//       <Text style={styles.message}>{item.message}</Text>
+//     </View>
+//   );
 const Softmax = () => {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
     return (
         <Provider store={store}>
             <Router />
+            <PushController />
         </Provider>
     )
 }
